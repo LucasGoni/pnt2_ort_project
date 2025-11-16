ea37df9de13f17f38063516d71991bcb0d9e2a5c
fd <fchiesah@gmail.com>
2025-11-16 13:05:16 -0300

    Se agrego calendario para perfil de alumno, como de entrenador

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,8 @@ import Login from "./pages/Login/Login.jsx";
 import Register from "./pages/Register/Register.jsx";
 import HomeEntrenador from "./pages/HomeEntrenador/HomeEntrenador";
 import HomeAlumno from "./pages/HomeAlumno/HomeAlumno";
+import CalendarAlumnoPage from "./pages/Calendar/CalendarAlumnoPage.jsx";
+import CalendarEntrenadorPage from "./pages/Calendar/CalendarEntrenadorPage.jsx";
 
 
 
@@ -15,6 +17,8 @@ function App() {
       <Route path="/entrenador" element={<HomeEntrenador />} />
       <Route path="/alumno" element={<HomeAlumno />} />
       <Route path="*" element={<h2 style={{ textAlign: "center" }}>404 - Página no encontrada</h2>} />
+      <Route path="/calendario/alumno" element={<CalendarAlumnoPage />} />
+      <Route path="/calendario/entrenador" element={<CalendarEntrenadorPage />} />
     </Routes>
   );
 }
